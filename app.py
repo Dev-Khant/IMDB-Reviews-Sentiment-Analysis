f248f852c507cad1fbe81e2c9da7d9dbeb04d066
Dev <devkhant24@gmail.com>
2020-06-26 16:26:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ def home():
 @app.route('/pred', methods=['POST'])
 def pred():
     sent = request.form['message']
-    sent = sent.lower()
-    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-    sent = pattern.sub('', sent)
-    sent = re.sub(r'[,.\"!@#$%^&*(){}?/;`~:<>+=-]', '', sent)
-    tokens = nltk.word_tokenize(sent)
-    table = str.maketrans('', '', string.punctuation)
-    stripped = [w.translate(table) for w in tokens]
-    words = [word for word in stripped if word.isalpha()]
-    stop_words = set(nltk.corpus.stopwords.words('english'))
-    stop_words.discard('not')
-    words = [Lemmatizer.lemmatize(word) for word in words if word not in stop_words]
-    words = ' '.join(words)
+#     sent = sent.lower()
+#     pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+#     sent = pattern.sub('', sent)
+#     sent = re.sub(r'[,.\"!@#$%^&*(){}?/;`~:<>+=-]', '', sent)
+#     tokens = nltk.word_tokenize(sent)
+#     table = str.maketrans('', '', string.punctuation)
+#     stripped = [w.translate(table) for w in tokens]
+#     words = [word for word in stripped if word.isalpha()]
+#     stop_words = set(nltk.corpus.stopwords.words('english'))
+#     stop_words.discard('not')
+#     words = [Lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+#     words = ' '.join(words)
     words = [sent]
     x = cv.transform(words).toarray()
     p = new_model.predict(x)
